66dee05d79dce26e89ce03d1d1641738be5cd9f2
INTESARSHAWON <intesarshawon@gmail.com>
2024-04-13 21:42:07 +0600

    button1 onclick

--- script.js
+++ script.js
@@ -4,3 +4,10 @@
 //ondblclick= "console.log ('button was clicked'); alert('hello');"
 //onmouseover= "console.log ('button was hoverd');"
 // eishob event inline a o handle kora jay, but JS er power beshi, dui jaygay code hole ekhane ja ashe tai ashbe
+
+
+let button1= document.querySelector(".button1");
+
+button1.onclick= ()=>{
+    console.log ("button was clicked");
+};
