338025a1fb3a8012435f549822f350ec869f0f9c
INTESARSHAWON <intesarshawon@gmail.com>
2024-04-13 21:42:41 +0600

    button2 dblclick

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ let button1= document.querySelector(".button1");
 button1.onclick= ()=>{
     console.log ("button was clicked");
 };
+
+
+let button2= document.querySelector(".button2");
+
+button2.ondblclick= ()=>{
+    console.log ("button was clicked");
+    alert ("yes or no");
+
+};
