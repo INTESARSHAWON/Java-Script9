4548dc18cae3777d03ea0967f2429a61c547cae7
INTESARSHAWON <intesarshawon@gmail.com>
2024-04-13 21:40:37 +0600

    some content about events

---
+++ script.js
@@ -0,0 +1,6 @@
+//mouse er dui tinta event e amra ekhlam MDN events a gele onk kichu ache, ja khub ekta use hoy na
+
+//onclick= "console.log ('button was clicked'); alert('hello');"
+//ondblclick= "console.log ('button was clicked'); alert('hello');"
+//onmouseover= "console.log ('button was hoverd');"
+// eishob event inline a o handle kora jay, but JS er power beshi, dui jaygay code hole ekhane ja ashe tai ashbe
